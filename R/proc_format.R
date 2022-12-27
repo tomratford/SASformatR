9785d6217be1bb84e6731007c2abc9800ebc7630
Tom Ratford <tomratford@gmail.com>
2022-12-27 14:37:22 +0000

    FEAT: create format catalog

--- R/proc_format.R
+++ R/proc_format.R
@@ -1,11 +1,31 @@
-#' Title
+#' Create a format catalog
 #'
-#' @param ...
+#' @param ... See `vignette("getting_started", package = "SASformatR")`
+#' @param catalog (`character`) Name of object to store format catalog in
 #'
-#' @return
+#' @return nothing, creates an object `formats` as a side effect
 #' @export
 #'
+#' @importFrom rlang list2
+#'
 #' @examples
-proc_format <- function(...) {
+#'proc_format(
+#' AGEGR1 = value(
+#'   "< 21" = "<21",
+#'   "21 - 39" = "21 - 39",
+#'   "40 <=" = ">=40"
+#' )
+#')
+proc_format <- function(..., catalog = "formats") {
+  if (exists(catalog, envir = .GlobalEnv)) {
+    partial <- get(catalog, envir = .GlobalEnv)
+  } else {
+    partial <- list()
+  }
 
+  newlist <- c(list2(...), partial)
+
+  assign(catalog,
+         newlist[!duplicated(names(newlist))],
+         envir = .GlobalEnv)
 }
--- tests/testthat/test-proc_format.R
+++ tests/testthat/test-proc_format.R
@@ -1,8 +1,67 @@
 # test functionality
-test_that("A catalog is created with nothing in", {
+test_that("a catalog is created with nothing in", {
   proc_format()
 
-  want <- new.env()
+  want <- list()
 
-  expect_equal(formats, )
+  expect_equal(formats, want)
+})
+
+test_that("a format is created in a catalog and can be indexed", {
+  proc_format(
+    PARAMCD = value(
+      "SYSBP" = "Systolic Blood Pressure",
+      "DIABP" = "Diastolic Blood Pressure",
+      "HR" = "Heart Rate"
+    )
+  )
+
+  want <- value(
+    "SYSBP" = "Systolic Blood Pressure",
+    "DIABP" = "Diastolic Blood Pressure",
+    "HR" = "Heart Rate"
+  )
+
+  expect_equal(formats$PARAMCD, want)
+})
+
+test_that("the same catalog can be added to multiple times", {
+  proc_format(
+    PARAMCD = value(
+      "SYSBP" = "Systolic Blood Pressure",
+      "DIABP" = "Diastolic Blood Pressure",
+      "HR" = "Heart Rate"
+    )
+  )
+
+  proc_format(
+    AGEGR1 = value(
+      "< 21" = "<21",
+      "21 - 39" = "21 - 39",
+      "40 <=" = ">=40"
+    )
+  )
+
+  want <- list(
+    AGEGR1 = value(
+      "< 21" = "<21",
+      "21 - 39" = "21 - 39",
+      "40 <=" = ">=40"
+    ),
+    PARAMCD = value(
+      "SYSBP" = "Systolic Blood Pressure",
+      "DIABP" = "Diastolic Blood Pressure",
+      "HR" = "Heart Rate"
+    )
+  )
+
+  expect_identical(formats, want)
+})
+
+test_that("catalogs can be named", {
+  proc_format(catalog = "myFormats")
+
+  want <- list()
+
+  expect_equal(myFormats, want)
 })
